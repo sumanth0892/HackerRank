856816ad035eb36b43a3713da973b6afb3dc044c
sumanth0892 <sumanth.murthy@liquidanalytics.com>
2019-11-03 19:49:05 -0500

    Add files via upload

---
+++ gridLand.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+def gridLand(n,m,k,track):
+    grid = np.zeros((n,m))
+    for t in track:
+        r,c1,c2 = t
+        r -= 1
+        c1 -= 1
+        c2 -= 1
+        grid[r,c1:c2+1] = 1
+    return np.count_nonzero(grid == 0)
+
+
+
+    
